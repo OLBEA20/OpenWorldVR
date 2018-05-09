bc4f01ec1d5d04a426ecd807ba70d2d82f2fb53c
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-05-09 14:15:02 -0400

    HandAnimation now tested

---
+++ OpenWorldVR/Assets/Editor/Script/Tests/Animation/HandAnimationTest.cs
@@ -0,0 +1,91 @@
+﻿using Assets.Script.Src;
+using Assets.Script.Src.Animation.Hand;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Editor.Script.Tests.Animation
+{
+    public class HandAnimationTest
+    {
+        private object _anObject;
+        private ClickedEventArgs _anEventArgs;
+
+        private IUnityAnimator _animator;
+        private SkinnedMeshRenderer _skinnedMeshRenderer;
+        private HandAnimation _handAnimation;
+
+        [SetUp]
+        public void BeforeEveryTest()
+        {
+            _animator = Substitute.For<IUnityAnimator>();
+            _skinnedMeshRenderer = Substitute.For<SkinnedMeshRenderer>();
+
+            _handAnimation = new HandAnimation(_animator, _skinnedMeshRenderer);
+            _anEventArgs = new ClickedEventArgs(); 
+            _anObject = new object();
+        }
+
+        [Test]
+        public void when_closing_hand_then_hand_is_closed()
+        {
+            _handAnimation.CloseHand();
+
+            _animator.Received().SetBool("close", true);
+        }
+
+        [Test]
+        public void when_opening_hand_then_hand_is_opened()
+        {
+            _handAnimation.OpenHand();
+
+            _animator.Received().SetBool("close", false);
+        }
+
+        [Test]
+        public void when_closing_three_fingers_then_three_fingers_should_be_closed()
+        {
+            _handAnimation.CloseThreeFingers(_anObject, _anEventArgs);
+
+            _animator.Received().SetBool("three_fingers_close", true);
+        }
+
+        [Test]
+        public void when_opening_theee_fingers_then_three_fingers_should_be_closed()
+        {
+            _handAnimation.OpenThreeFingers(_anObject, _anEventArgs);
+
+            _animator.Received().SetBool("three_fingers_close", false);
+        }
+
+        [Test]
+        public void when_closing_index_then_index_is_closed()
+        {
+            _handAnimation.CloseIndex(_anObject, _anEventArgs);
+
+            _animator.Received().SetBool("close_index", true);
+        }
+        [Test]
+        public void when_opening_index_then_index_is_opened()
+        {
+            _handAnimation.OpenIndex(_anObject, _anEventArgs);
+
+            _animator.Received().SetBool("close_index", false);
+        }
+
+        [Test]
+        public void when_closing_thumb_then_thumb_should_be_closed()
+        {
+            _handAnimation.CloseThumb(_anObject, _anEventArgs);
+
+            _animator.Received().SetBool("close_thumb", true);
+        }
+        [Test]
+        public void when_opening_thumb_then_thumb_is_opened()
+        {
+            _handAnimation.OpenThumb(_anObject, _anEventArgs);
+
+            _animator.Received().SetBool("close_thumb", false);
+        }       
+    }
+}
---
+++ OpenWorldVR/Assets/Script/Src/Animation/Hand/HandAnimationInput.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Assets.Script.Src.Animation.Hand
+{
+    public class HandAnimationInput: MonoBehaviour {
+
+        public SteamVR_TrackedController Controller;
+
+        private UnityAnimator _animator;
+        private SkinnedMeshRenderer _skinnedMeshRenderer;
+        private HandAnimation _handAnimation;
+
+        void Awake()
+        {
+            _animator = new UnityAnimator(GetComponent<Animator>());
+            _skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+            _handAnimation = new HandAnimation(_animator, _skinnedMeshRenderer);
+        }
+
+        void OnEnable()
+        {
+            Controller.Gripped += _handAnimation.CloseThreeFingers;
+            Controller.Ungripped += _handAnimation.OpenThreeFingers;
+
+            Controller.TriggerClicked += _handAnimation.CloseIndex;
+            Controller.TriggerUnclicked += _handAnimation.OpenIndex;
+
+            Controller.PadTouched += _handAnimation.CloseThumb;
+            Controller.PadUntouched += _handAnimation.OpenThumb;
+        }
+
+        void OnDisable()
+        {
+            Controller.Gripped -= _handAnimation.CloseThreeFingers;
+            Controller.Ungripped -= _handAnimation.OpenThreeFingers;
+
+            Controller.TriggerClicked -= _handAnimation.CloseIndex;
+            Controller.TriggerUnclicked -= _handAnimation.OpenIndex;
+
+            Controller.PadTouched += _handAnimation.CloseThumb;
+            Controller.PadUntouched += _handAnimation.OpenThumb;
+        }
+    }
+}
